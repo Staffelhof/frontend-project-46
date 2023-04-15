5c6c9af047ea2dc6c40494ad2f62e8416ebf8fc5
Staffelhof <akojanchikov@gmail.com>
2023-04-16 00:12:37 +0300

    rework

--- src/formatters/plainFormatter.js
+++ src/formatters/plainFormatter.js
@@ -10,35 +10,33 @@ const convertToString = (value) => {
   return value;
 };
 
-const makePlainDiff = (diff) => {
-  const iter = (nest, path = []) => {
-    const sorted = _.sortBy(Object.entries(nest));
-    const lines = sorted.reduce((acc, [key, value]) => {
-      const currentPath = [...path, key];
-      const { status } = value || { };
+const iter = (nest, path = []) => {
+  const sorted = _.sortBy(Object.entries(nest));
+  const lines = sorted.reduce((acc, [key, value]) => {
+    const currentPath = [...path, key];
+    const { status } = value || { };
 
-      if (status) {
-        const returnValue = convertToString(value.value);
-        const returnValue2 = convertToString(value.value2);
-        const possibleStatus = {
-          added: `was added with value: ${returnValue}`,
-          deleted: 'was removed',
-          changed: `was updated. From ${returnValue} to ${returnValue2}`,
-        };
+    if (status) {
+      const returnValue = convertToString(value.value);
+      const returnValue2 = convertToString(value.value2);
+      const possibleStatus = {
+        added: `was added with value: ${returnValue}`,
+        deleted: 'was removed',
+        changed: `was updated. From ${returnValue} to ${returnValue2}`,
+      };
 
-        if (status === 'equal') {
-          return [...acc];
-        }
-        return [...acc, `Property '${currentPath.join('.')}' ${possibleStatus[status]}`];
+      if (status === 'equal') {
+        return [...acc];
       }
+      return [...acc, `Property '${currentPath.join('.')}' ${possibleStatus[status]}`];
+    }
 
-      return [...acc, iter(value, currentPath)];
-    }, []);
+    return [...acc, iter(value, currentPath)];
+  }, []);
 
-    return lines.join('\n');
-  };
-
-  return iter(diff);
+  return lines.join('\n');
 };
+
+const makePlainDiff = (diff) => iter(diff);
 
 export default makePlainDiff;
--- src/formatters/stylishFormatter.js
+++ src/formatters/stylishFormatter.js
@@ -8,36 +8,34 @@ const diffSymbol = {
   equal: '    ',
 };
 
-const makeStylishDiff = (diff) => {
-  const iter = (nest, depth) => {
-    if (!_.isPlainObject(nest)) {
-      return `${nest}`;
+const iter = (nest, depth) => {
+  if (!_.isPlainObject(nest)) {
+    return `${nest}`;
+  }
+
+  const endBracketPadding = spaces.repeat(depth);
+  const padding = `${spaces.repeat(depth + 1)}`;
+  const sorted = _.sortBy(Object.entries(nest));
+
+  const lines = sorted.reduce((acc, [key, value]) => {
+    const lineMaker = (k = key, v = value, p = padding) => `${p}${k}: ${iter(v, depth + 1)}`;
+    const { status } = value || { };
+
+    if (status === 'changed') {
+      const newPadding = `${spaces.repeat(depth)}${diffSymbol.deleted}`;
+      const newPadding2 = `${spaces.repeat(depth)}${diffSymbol.added}`;
+      return [...acc, lineMaker(key, nest[key].value, newPadding),
+        lineMaker(key, nest[key].value2, newPadding2)];
     }
-
-    const endBracketPadding = spaces.repeat(depth);
-    const padding = `${spaces.repeat(depth + 1)}`;
-    const sorted = _.sortBy(Object.entries(nest));
-
-    const lines = sorted.reduce((acc, [key, value]) => {
-      const lineMaker = (k = key, v = value, p = padding) => `${p}${k}: ${iter(v, depth + 1)}`;
-      const { status } = value || { };
-
-      if (status === 'changed') {
-        const newPadding = `${spaces.repeat(depth)}${diffSymbol.deleted}`;
-        const newPadding2 = `${spaces.repeat(depth)}${diffSymbol.added}`;
-        return [...acc, lineMaker(key, nest[key].value, newPadding),
-          lineMaker(key, nest[key].value2, newPadding2)];
-      }
-      if (status) {
-        const newPadding = `${spaces.repeat(depth)}${diffSymbol[status]}`;
-        return [...acc, lineMaker(key, nest[key].value, newPadding)];
-      }
-      return [...acc, lineMaker()];
-    }, []);
-    return ['{', ...lines, `${endBracketPadding}}`].join('\n');
-  };
-
-  return iter(diff, 0, []);
+    if (status) {
+      const newPadding = `${spaces.repeat(depth)}${diffSymbol[status]}`;
+      return [...acc, lineMaker(key, nest[key].value, newPadding)];
+    }
+    return [...acc, lineMaker()];
+  }, []);
+  return ['{', ...lines, `${endBracketPadding}}`].join('\n');
 };
+
+const makeStylishDiff = (diff) => iter(diff, 0, []);
 
 export default makeStylishDiff;
