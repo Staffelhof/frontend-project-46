d0c6560e40b518a3e8a05e0f74b0f06939832c9f
Staffelhof <akojanchikov@gmail.com>
2023-05-17 20:34:35 +0300

    rework

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
-import genDiff from '../src/index.js';
-import {jsonResult, plainResult, stylishNestedResult} from "../__fixtures__/results.js";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import genDiff from '../src/index.js';
+import { jsonResult, plainResult, stylishNestedResult } from '../__fixtures__/results.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-export const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-const extensions = ['json', 'yaml']
+const extensions = ['json', 'yaml'];
 let stylishExpected;
 let plainExpected;
 let jsonExpected;
@@ -16,11 +16,11 @@ beforeAll(() => {
   stylishExpected = stylishNestedResult;
   plainExpected = plainResult;
   jsonExpected = jsonResult;
-})
-test.each(extensions)(`[%# TEST] %s format test for nested fixtures`, (ext) => {
+});
+test.each(extensions)('[%# TEST] %s format test for nested fixtures', (ext) => {
   const fixturePath = getFixturePath(`nested1.${ext}`);
   const fixturePath1 = getFixturePath(`nested2.${ext}`);
   expect(genDiff(fixturePath, fixturePath1, 'stylish')).toEqual(stylishExpected);
   expect(genDiff(fixturePath, fixturePath1, 'plain')).toEqual(plainExpected);
-  expect(genDiff(fixturePath, fixturePath1, 'json',)).toEqual(jsonExpected);
+  expect(genDiff(fixturePath, fixturePath1, 'json')).toEqual(jsonExpected);
 });
